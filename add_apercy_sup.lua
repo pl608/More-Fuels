2cc33bc56517365e5d488ed54ba8bea835efef24
pl608 <78400949+pl608@users.noreply.github.com>
2021-06-21 19:49:33 -0700

    added some things...
    hope i spelled nautilus right!

--- add_apercy_sup.lua
+++ add_apercy_sup.lua
@@ -13,6 +13,18 @@ function boat()
 		motorboat.fuel = {['biofuel:biofuel'] = 1,['biofuel:bottle_fuel'] = 1,['biofuel:phial_fuel'] = 0.25, ['biofuel:fuel_can'] = 5, ["more_fuels:gasoline"] = 10}
 	end
 end
+function submarine()
+	if rawget(_G, "nautilus") and nautilus.fuel then
+		nautilus.fuel = {['biofuel:biofuel'] = 1,['biofuel:bottle_fuel'] = 1,['biofuel:phial_fuel'] = 0.25, ['biofuel:fuel_can'] = 5, ["more_fuels:gasoline"] = 10}
+	end
+end
+function copter()
+	if rawget(_G, "helicopter") and helicopter.fuel then
+		helicopter.fuel = {['biofuel:biofuel'] = 1,['biofuel:bottle_fuel'] = 1,['biofuel:phial_fuel'] = 0.25, ['biofuel:fuel_can'] = 5, ["more_fuels:gasoline"] = 10}
+	end
+end
 if minetest.get_modpath("hidroplane") then hydro() end
 if minetest.get_modpath("trike") then trike() end
 if minetest.get_modpath("motorboat") then boat() end
+if minetest.get_modpath("nautilus") then submarine() end
+if minetest.get_modpath("helicopter") then copter() end
