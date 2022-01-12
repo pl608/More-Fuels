d47ebb52dd351e866d11a9f01c880f220fa9746c
pl608 <78400949+pl608@users.noreply.github.com>
2022-01-12 12:03:19 -0800

    This probably wont work... but its fun :)

--- crafting.lua
+++ crafting.lua
@@ -4,7 +4,11 @@ minetest.register_craftitem("more_fuels:refined_oil", {
 	inventory_image = "bucket_oil_refined.png",
 	stack_max = 1,
 })
-
+--butain can(empty)
+minetest.register_craftitem("more_fuels:butain_canister_emtpy", {
+	description = "Empty Butain Canister",
+	inventory_image = "butain_empty.png"
+}
 --regular oil
 minetest.register_craft({
 	type = "cooking",
@@ -19,7 +23,21 @@ minetest.register_craft({
 	burntime = 300,
 	replacements = {{"more_fuels:bucket_oil", "bucket:bucket_empty"}},
 })
-
+--Butain
+minetest.register_craft({
+	type = "fuel",
+	recipe = {"more_fuels:ultra_refined_oil","more_fuels:butain_canister_emtpy"},
+	replacements = {{"more_fuels:butain_canister_emtpy","more_fuels:butain_canister"}},
+	burntime = 555
+})
+minetest.register_craft({
+	output = "more_fuels:butain_canister_emtpy",
+	recipe = {
+			{"","default:steel_ingot",""},
+			{"default:steel_ingot","","default:steel_ingot"},
+			{"default:steel_ingot","default:steel_ingot","default:steel_ingot"},
+		 }
+})
 --gasoline
 minetest.register_craft({
 	type = "fuel",
