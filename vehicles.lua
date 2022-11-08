9594591f7da40be2546d9382ebab3e078c0ffe3b
pl608 <78400949+pl608@users.noreply.github.com>
2022-11-08 14:25:26 -0800

    added more vehicles

--- vehicles.lua
+++ vehicles.lua
@@ -28,8 +28,29 @@ function copter()
 	end
 end
 
+function ju52()
+	if rawget(_G, "ju52") and ju52.fuel then
+		ju52.fuel = {['biofuel:biofuel'] = 1,['biofuel:bottle_fuel'] = 1,['biofuel:phial_fuel'] = 0.25, ['biofuel:fuel_can'] = 5, ["more_fuels:gasoline"] = 10}
+	end
+end
+
+function pa28()
+	if rawget(_G, "pa28") and pa28.fuel then
+		pa28.fuel = {['biofuel:biofuel'] = 1,['biofuel:bottle_fuel'] = 1,['biofuel:phial_fuel'] = 0.25, ['biofuel:fuel_can'] = 5, ["more_fuels:gasoline"] = 10}
+	end
+end
+
+function demoiselle()
+	if rawget(_G, "demoiselle") and demoiselle.fuel then
+		demoiselle.fuel = {['biofuel:biofuel'] = 1,['biofuel:bottle_fuel'] = 1,['biofuel:phial_fuel'] = 0.25, ['biofuel:fuel_can'] = 5, ["more_fuels:gasoline"] = 10}
+	end
+end
 if minetest.get_modpath("hidroplane") then hydro() end
 if minetest.get_modpath("trike") then trike() end
 if minetest.get_modpath("motorboat") then boat() end
 if minetest.get_modpath("nautilus") then submarine() end
 if minetest.get_modpath("helicopter") then copter() end
+if minetest.get_modpath("ju52") then ju52() end
+if minetest.get_modpath("pa28") then pa28() end
+if minetest.get_modpath("pa28") then pa28() end
+if minetest.get_modpath("demoiselle") then demoiselle() end
